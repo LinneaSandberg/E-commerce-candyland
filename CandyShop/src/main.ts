d2a1e7c42a032cc273ef783acb142bd232022062
Oscar Throedsson <oscar.throedsson@gmail.com>
2023-12-23 09:36:27 +0100

    Logic för localstorage är fixat

--- CandyShop/src/main.ts
+++ CandyShop/src/main.ts
@@ -1,15 +1,12 @@
 import './style.css'
 import {productCard} from "./productCard.ts"
-
 import navBar from './navBar'
-
 
 async function renderScreen(){
   document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   ${navBar()}
   <main>
   ${await productCard()}
- 
   <main>
   <div>
     <h1>The file is clean 🛀🏽</h1>

7e76057376501a01e98dfa65674fd7a19764820e
Linnea Sandberg <llinnea.sandberg@gmail.com>
2023-12-27 08:59:07 +0100

    function to show the cart with prodct and total amounts of prices

--- CandyShop/src/cart.ts
+++ CandyShop/src/cart.ts
@@ -5,8 +5,12 @@ interface ProductToCart {
     price: number
 }
 
+const cart: ProductToCart[] = [];
+
 const toShowCartEl = document.querySelector<HTMLDivElement>('.itemAmountInCart')!
 const asideForOrderEl = document.querySelector<HTMLDivElement>('#order')!
+const cartElementEl = document.querySelector<HTMLUListElement>('#cartList')!
+const orderTotalEl = document.querySelector<HTMLUListElement>('#orderTotal')!
 
 
 
@@ -15,16 +19,33 @@ const renderCart = () => {
     asideForOrderEl.innerHTML = `
     <h2>Shoppingbag:</h2>
     <ul id="cartList"></ul>
+    <p id="orderTotal">Order total: </p>
     `
 }
 
 
+function pushToCart(product: ProductToCart) {
+    cart.push(product);
 
- // Funktion för att lägga till produkter i varukorgen
- function addToCart(product: ProductToCart) {
-    const cartItem = document.createElement("li");
-    cartItem.textContent = product.name + " - " + product.price + " kr";
-    document.querySelector<HTMLUListElement>('#cartList')!.appendChild(cartItem);
+    updateCart();
+}
+
+
+// function to show both the products and the prices for each product in the basket
+function updateCart() {
+    let total = 0;
+    cart.forEach(product => {
+        const cartItem = document.createElement("li");
+        const productTotal = product.price;
+        total += productTotal;
+
+        cartItem.textContent = `${product.name} - ${productTotal} kr`;
+        cartElementEl.appendChild(cartItem);
+    })
+
+    orderTotalEl.innerHTML = `Totalcost: ${total} kr`
+
+
 }
 
 

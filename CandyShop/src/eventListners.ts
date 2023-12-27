6ace37ab195edc2b00264cecd5970b1c228f12a5
Oscar Throedsson <oscar.throedsson@gmail.com>
2023-12-27 11:19:11 +0100

    Lämnat anteckningar till johan

--- CandyShop/src/eventListners.ts
+++ CandyShop/src/eventListners.ts
@@ -5,18 +5,39 @@
 // hänsyn till asynkron metoder
 
 export function setListeners(){
+  //Hämtar alla eraseknappar
   const eraseButtons = document.querySelectorAll("#eraseFromCart") as NodeListOf<HTMLButtonElement>; //hämtar alla knappar som tar bort godis 
-  console.log(eraseButtons);
+
+
+  //TODO hämta ut alla knappar som ska lägga till godis i cart 
+ 
+
+
+  //Lägger  en knapp på alla click event på alla eraseknappar
 eraseButtons.forEach((eraseBtn)=>{
   eraseBtn.addEventListener("click", (event)=>{
-    console.log(`knapp ${eraseBtn} var klickad `)
+    // Behöver få ut ID på godiset som användaren har klickat på
+    // Kolla om det finns i cart i  localStorage och ta bort 1st
   })
 })
 }
 
 
-// yttersta div: class: productCard
-//footer -> knapparna 
-// -- minusknapp: addToCartContainer
-// ++ plusknapp: addToCart
-//visa hur många cart item: itemInCart
+//Sätta eventlyssnare på alla knappar som ska lägga till godis i cart
+  //Du behöver lista ut hur du tar ut följande information om godiset på det produktkort som har fått ett event (click)
+  /*
+  interface ProductItem{ 
+  id: number, <- godis ID
+  image: string <- godis URL
+  name: string <- Godiset namn
+  price: number <- Priset för godiset
+  stock: number <- Hur många som är i stock // osäker låt mig tänka lite här varför du behöver skicka detta. 
+  } */
+
+// skicka in objectet till addProductShoppingCart()
+// nu ska det inte behöva göras mer
+
+
+// ------------------ TIPS ------------------------
+
+  // Du behöver ha koll på localStorageLogic tar emot och gör. 

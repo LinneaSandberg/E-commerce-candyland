b56ef6b9de974ec0bbd032c71922d38facce0181
Oscar Throedsson <oscar.throedsson@gmail.com>
2023-12-27 11:33:44 +0100

    Lämnat anteckningar till johan och löst lägga till knappen ej sync med local storage

--- CandyShop/src/eventListners.ts
+++ CandyShop/src/eventListners.ts
@@ -1,3 +1,6 @@
+
+
+
 // vi ska hitta HTML produkkorten
 // hänsyn till asynkron metoder
 
@@ -19,8 +22,22 @@ eraseButtons.forEach((eraseBtn)=>{
 })
 }
 
-// yttersta div: class: productCard
-//footer -> knapparna 
-// -- minusknapp: addToCartContainer
-// ++ plusknapp: addToCart
-//visa hur många cart item: itemInCart
+
+//Sätta eventlyssnare på alla knappar som ska lägga till godis i cart
+  //Du behöver lista ut hur du tar ut följande information om godiset på det produktkort som har fått ett event (click)
+  /*
+  interface ProductItem{ 
+  id: number, <- godis ID
+  image: string <- godis URL
+  name: string <- Godiset namn
+  price: number <- Priset för godiset
+  stock: number <- Hur många som är i stock // osäker låt mig tänka lite här varför du behöver skicka detta. 
+  } */
+
+// skicka in objectet till addProductShoppingCart()
+// nu ska det inte behöva göras mer
+
+
+// ------------------ TIPS ------------------------
+
+  // Du behöver ha koll på localStorageLogic tar emot och gör. 

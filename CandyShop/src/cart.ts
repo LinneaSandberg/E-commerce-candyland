bb1b8a81546bb7c7ee93e1555c9d09f2590f0df2
Oscar Throedsson <oscar.throedsson@gmail.com>
2023-12-27 12:34:28 +0100

    Funktionen renderCart kan kolla i localstorage

--- CandyShop/src/cart.ts
+++ CandyShop/src/cart.ts
@@ -1,74 +1,78 @@
-// interface ProductToCart {
-//     id: number
-//     name: string
-//     price: number
-// }
-// import {getCart} from "./localStorageLogic"
-// const cart: ProductToCart[] = [];
-console.log("hej")
-// const buttonCartEl = document.querySelector<HTMLDivElement>('.bajs')!;
+
+// Funktionen som hämtar allt som finns i cart i localstorage
+import {getCart} from "./localStorageLogic"
+
+
 
 const mainEL = document.querySelector<HTMLDivElement>('#app')!;
 
-
-
-// const orderTotalEl = document.querySelector<HTMLUListElement>('#orderTotal')!;
 
 //Öppna cart vy
 export function cartListener(){
 const cartElementEl = document.querySelector<HTMLDivElement>('.bajs')!;
 
 cartElementEl.addEventListener("click", (event)=>{
+
+        // hämtar vi cart items från local storage
+         renderCart()
+
+
         // ny ska vyn för aside renderas
-        mainEL.innerHTML= `
-        <aside>
-        designa ASIDE
-        </aside>
-        `
+        // mainEL.innerHTML+= `
+        // <aside>
+        // //rendera ut våran cart i localstorage
+        // </aside>
+        // `
     })
 
 }
 
 
 
-// export function openCart() {
-  
-//     buttonCartEl.addEventListener('click', async (e) => {
 
-//             e.preventDefault();
-        
-//             console.log('klickade på rätt knapp!')
-//             renderCart();
-//         })
+const renderCart = () => {
+const cartItems = getCart();
+console.log("cartItems:", cartItems)
 
-// }
-
-
-
-
-// export const renderCart = () => {
-// const cartItems = getCart();
-// console.log()
-    //hämta från cart 
-    //rendera ut från cart 
-
-
-
-
-    // mainEL.innerHTML = `
-    // <h2>Shoppingbag:</h2>
-    // <ul id="cartList"></ul>
-    // <p id="orderTotal">Order total: </p>
-    // `
-// }
+}
 
 
 // //function to add items to the cart
-// function pushToCart(product: ProductToCart) {
-//     cart.push(product);
 
-//     updateCart();
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // // function to show both the products and the prices for each product in the basket

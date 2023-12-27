39ed84b5f603ef943be7005023327e4a3e240a8b
Oscar Throedsson <oscar.throedsson@gmail.com>
2023-12-27 15:47:32 +0100

    fixade nav i main.ts

--- CandyShop/src/main.ts
+++ CandyShop/src/main.ts
@@ -1,12 +1,21 @@
 import "./style.css";
 import { productCard } from "./productCard.ts";
 import {setListeners} from "./eventListners.ts"
-import navBar from "./navBar";
+
 
 
 async function renderScreen(){
   document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  ${navBar()}  
+  <nav>
+    <p>CandyShop</p>
+   
+    <div class="bajs">
+        <i class="bi bi-cart2"></i>
+        <div class="itemAmountInCart">0<div>
+    </div>
+ 
+  </nav>
+ 
 <main id="toRender">
   ${await productCard()}
  

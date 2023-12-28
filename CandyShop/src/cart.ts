8ced631c518b2afecf64668d484db5af21adb5b8
Linnea Sandberg <llinnea.sandberg@gmail.com>
2023-12-28 15:18:27 +0100

    html for aside with cart done, started working on the styles

--- CandyShop/src/cart.ts
+++ CandyShop/src/cart.ts
@@ -45,26 +45,29 @@ const cartItems = getCart();
 
 // cartItem kommer vara en array som följer interfacet i CartItem[]:ProductItem + extends 
 return `
-<div class="cartItemsWrapper"
-    <table>
-        <thead>
-            <tr>
-                <th>Your shoppingbag 🛒</th>
-        </thead>
+<div id="cartItemsWrapper">
+    <table id="tableBox">
+    <button></button>
+     <caption id="capTitle">Your shoppingbag 🛒</caption>
+    <thead>
+     <tr>
+      <th>Products in you basket:</th>
+     </tr>
+    </thead>
     <tbody>
         ${
             cartItems.map((cartItem: CartItem )=>{
             return`
                 <tr>
-                    <td>Product: ${cartItem.name}</td>
-                    <td>Am                        <button>
-                         <i class="bi bi-arrow-up-short"></i>
-                        </button>
-                        <button>
-                         <i class="bi bi-arrow-up-short"></i>
-                        </button>
+                    <th>Product: ${cartItem.name}</th>
+                    <td>
+                    <button>
+                     <i class="bi bi-arrow-up-short"></i>
+                    </button>
+                    <button>
+                     <i class="bi bi-arrow-up-short"></i>
+                    </button>
                      </td>
-
                      <td>Total: ${cartItem.totalCost}</td>
                 </tr>
         `
@@ -73,11 +76,13 @@ return `
 </tbody>
 <tfoot>
 <tr>
-<td>Total amount off products: ${cartItems.amount}</th>
-<td>Totalcost off order: ${cartItems.totalCost}</td>
+<th colspan="3">Total amount off products: ${cartItems.amount}</th>
 </tr>
 <tr>
-<td>
+<td colspan="3">Totalcost off order: ${cartItems.totalCost}</td>
+</tr>
+<tr>
+<td colspan="3">
 <button id="checkout">Checkout</button>
 </td>
 </tr>

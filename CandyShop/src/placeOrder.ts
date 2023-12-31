14b54877b16342c091e3e382d2ef6c491fe09226
Linnea Sandberg <llinnea.sandberg@gmail.com>
2023-12-31 15:05:55 +0100

    styling for orderchekout

--- CandyShop/src/placeOrder.ts
+++ CandyShop/src/placeOrder.ts
@@ -10,7 +10,6 @@ const zipcodeInputEl = document.querySelector<HTMLInputElement>('#zipcodeInput')
 const cityInputEl = document.querySelector<HTMLInputElement>('#cityInput');
 const telInputEl = document.querySelector<HTMLInputElement>('#telInput');
 const mailInputEl = document.querySelector<HTMLInputElement>('#mailInput');
-const mainEL = document.querySelector<HTMLDivElement>('#app')!;
 
 
 const submitOrder = async (placeOrder: placeOrder) => {
@@ -91,80 +90,79 @@ export const renderOrder = () => {
     cartItems?.forEach((total) => {
       totalProduct += total.amount;
     });
-    const antal = totalProduct === 1 ? "Vara" : "Varor";
+    const antal = totalProduct === 1 ? "vara" : "varor";
 
 
     return `
-
 <header class="header">
 <h2>Kassa</h2>
 <p>Orderinfo</p>
 </header>
 
 <section class="itemCardWrapper">
-${cartItems?.map((cartItem: CartItem) => {
+${cartItems
+    ?.map((cartItem: CartItem) => {
     const antal = cartItem.amount === 1 ? "vara" : "varor";
     return `
     <div class="itemCheckout">
-    <div class="boxOne">
-     <figure class="iconCheckout">
-      <i class="bi bi-bag-fill"></i>
-     </figure>
+     <div class="boxOne">
+      <figure class="iconCheckout">
+       <i class="bi bi-bag-fill"></i>
+      </figure>
+     </div>
+     <div class="boxTwo">
+      <ul class="checkoutList">
+       <li class="productTitle">${cartItem.name}</li>
+       <li class="productAmount">${cartItem.amount} ${antal}</li>
+       <li class="productSum">Totalt ${cartItem.totalCost} kr</li>
+      </ul>
+     </div>
     </div>
-    
-    <div class="boxTwo">
-     <ul class="checkoutList">
-      <li class="productTitle">${cartItem.name}</li>
-      <li class="productAmount">${cartItem.amount} ${antal}</li>
-      <li class="productSum">Totalt ${cartItem.totalCost} kr</li>
-     </ul>
-    </div>
-    </div>
-    `
-})}
+    `;
+}).join("")}
 </section>
 
 <div class="sumwrapper">
-<p class="checkoutPara">Totalt ${totalProduct} ${antal}</p>
-<figure class="iconCheckout">
+ <p class="totalProduct">Totalt ${totalProduct} ${antal}</p>
+ <figure class="iconCheckout iconTotal">
       <i class="bi bi-bag-fill"></i>
-</figure>
-<p class="checkoutPara">Totalsumma: ${totalPrice} kr</p>
+ </figure>
+ <p class="totalPrice">Totalsumma: ${totalPrice} kr</p>
 </div>
 
 <form id="orderForm" action="http://www.bortakvall.se/api/v2/users/31/orders" method="post">
-
-    <label for="nameInput">
-        Name:
+ <div class="inputWrapper">
+    <label for="nameInput" class="underline">
+        Namn:
         <input type="text" name="namn" id="nameInput" required>
     </label>
 
-    <label for="adressInput">
+    <label for="adressInput" class="underline">
         Adress:
         <input type="text" name="adress" id="adressInput" required>
     </label>
 
-    <label for="zipcodeInput">
-        Zipcode:
+    <label for="zipcodeInput" class="underline">
+        Postnummer:
         <input type="text" name="zipcode" id="zipcodeInput" required>
     </label>
 
-    <label for="cityInput">
-        City:
+    <label for="cityInput" class="underline">
+        Ort:
         <input type="text" name="ort" id="cityInput" required>
     </label>
 
     <label for="telInput">
-        Telephone:
+        Telefon:
         <input type="text" name="telephone" id="telInput">
     </label>
 
-    <label for="mailInput">
-        Email:
+    <label for="mailInput" class="underline">
+        E-post:
         <input type="text" name="email" id="mailInput" required>
     </label>
-
-    <button type="submit">Lägg order</button>
+ </div>
+ <button type="submit">Lägg order</button>
 </form>`
 
 }

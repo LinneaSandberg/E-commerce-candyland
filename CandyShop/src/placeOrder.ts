0f30c0a0568dda93b32bac52ebeb9987115a10aa
Johan Kremer <johan.h.kremer@gmail.com>
2024-01-03 13:31:07 +0100

    Stuff

--- CandyShop/src/placeOrder.ts
+++ CandyShop/src/placeOrder.ts
@@ -1,38 +1,38 @@
 import { ApiResponse, CartItem, Data } from "./interface";
 import { getCart } from "./localStorageLogic";
-import { sendOrder } from './apiCalls';
+import { sendOrder } from "./apiCalls";
 
-// 
+//
 function objApi() {
-    const cart = getCart();
-    const orderItems = cart?.map((product) => {
-        return {
-            product_id: product.id,
-            qty: product.amount,
-            item_price: product.price,
-            item_total: product.totalCost,
-        };
-    })
-    return orderItems;
+  const cart = getCart();
+  const orderItems = cart?.map((product) => {
+    return {
+      product_id: product.id,
+      qty: product.amount,
+      item_price: product.price,
+      item_total: product.totalCost,
+    };
+  });
+  return orderItems;
 }
 
 // funktion för att rendera orderinfo + formulär
 export function renderOrder() {
-    const wrapper = document.querySelector<HTMLDivElement>("#cartItemsWrapper")!;
-    const cartItems = getCart();
-    
-    let totalPrice: number = 0;
-    cartItems?.forEach((total) => {
-    totalPrice += total.totalCost;
-    });
-  
-    let totalProduct: number = 0;
-    cartItems?.forEach((total) => {
-      totalProduct += total.amount;
-    });
-    const antal = totalProduct === 1 ? "vara" : "varor";
+  const wrapper = document.querySelector<HTMLDivElement>("#cartItemsWrapper")!;
+  const cartItems = getCart();
 
-    wrapper.innerHTML= `
+  let totalPrice: number = 0;
+  cartItems?.forEach((total) => {
+    totalPrice += total.totalCost;
+  });
+
+  let totalProduct: number = 0;
+  cartItems?.forEach((total) => {
+    totalProduct += total.amount;
+  });
+  const antal = totalProduct === 1 ? "vara" : "varor";
+
+  wrapper.innerHTML = `
 <header>
 <h2>Kassa</h2>
 <p>Orderinfo</p>
@@ -76,85 +76,94 @@ export function renderOrder() {
     </label>
  </div>
  <button type="submit">Lägg order</button>
-</form>`
+</form>`;
 
-placeOrder(totalPrice);
+  placeOrder(totalPrice);
 }
 
 // funktion för att skicka order med inputvärderna till api
-function placeOrder(totalPrice: number){
-const orderFormEl = document.querySelector<HTMLFormElement>('#orderForm');
+function placeOrder(totalPrice: number) {
+  const orderFormEl = document.querySelector<HTMLFormElement>("#orderForm");
 
-//----------------------------------------------
-orderFormEl?.addEventListener("submit", async (e) => {
+  //----------------------------------------------
+  orderFormEl?.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-   // här hämtar jag min DOM referenser
-   const firstNameEl = document.querySelector<HTMLInputElement>('#firstName');
-   const lastNameEl = document.querySelector<HTMLInputElement>('#lastName');
-   const adressInputEl = document.querySelector<HTMLInputElement>('#adressInput');
-   const zipcodeInputEl = document.querySelector<HTMLInputElement>('#zipcodeInput');
-   const cityInputEl = document.querySelector<HTMLInputElement>('#cityInput');
-   const mailInputEl = document.querySelector<HTMLInputElement>('#mailInput');
-   
+    // här hämtar jag min DOM referenser
+    const firstNameEl = document.querySelector<HTMLInputElement>("#firstName");
+    const lastNameEl = document.querySelector<HTMLInputElement>("#lastName");
+    const adressInputEl =
+      document.querySelector<HTMLInputElement>("#adressInput");
+    const zipcodeInputEl =
+      document.querySelector<HTMLInputElement>("#zipcodeInput");
+    const cityInputEl = document.querySelector<HTMLInputElement>("#cityInput");
+    const mailInputEl = document.querySelector<HTMLInputElement>("#mailInput");
+
     // input värden för alla inputfält
     const firstName = firstNameEl?.value || "";
     const lastName = lastNameEl?.value || "";
     const adressInput = adressInputEl?.value || "";
     const zipcodeInput = zipcodeInputEl?.value || "";
-    const cityInput = cityInputEl?.value ||"";
-    const mailInput = mailInputEl?.value ||"";
+    const cityInput = cityInputEl?.value || "";
+    const mailInput = mailInputEl?.value || "";
 
     const cart = objApi();
 
-   // ett object med beställaren inputs
-   const placeOrder: Data = {
-   customer_first_name: firstName,
-   customer_last_name: lastName,
-   customer_address: adressInput,
-   customer_postcode: zipcodeInput,
-   customer_city: cityInput,
-   customer_email: mailInput,
-   order_total: totalPrice,
-   order_items: cart
-   }
+    // ett object med beställaren inputs
+    const placeOrder: Data = {
+      customer_first_name: firstName,
+      customer_last_name: lastName,
+      customer_address: adressInput,
+      customer_postcode: zipcodeInput,
+      customer_city: cityInput,
+      customer_email: mailInput,
+      order_total: totalPrice,
+      order_items: cart,
+    };
 
-   // skapa en if-sats som kollar att alla input med requiered är ifyllda
-   if (!firstName || !lastName || !adressInput || !zipcodeInput || !cityInput || !mailInput) {
-       alert("Please fill in all required fields");
-       return;
-   }
+    // skapa en if-sats som kollar att alla input med requiered är ifyllda
+    if (
+      !firstName ||
+      !lastName ||
+      !adressInput ||
+      !zipcodeInput ||
+      !cityInput ||
+      !mailInput
+    ) {
+      alert("Please fill in all required fields");
+      return;
+    }
 
-   //Testar göra ett API inrop. catch hanterar om det inte går att anropa APIet
-   try {
-    const response = await sendOrder(placeOrder);
-   
-    renderStatusSuccess(response);
-   } catch (error) {
-    renderStatusFail();
-   }
-});
+    //Testar göra ett API inrop. catch hanterar om det inte går att anropa APIet
+    try {
+      const response = await sendOrder(placeOrder);
+
+      renderStatusSuccess(response);
+    } catch (error) {
+      renderStatusFail();
+    }
+  });
 }
 
 // funktion som renderar HTML för att ordern gick igenom
 const renderStatusSuccess = (data: ApiResponse) => {
-    const wrapper = document.querySelector<HTMLDivElement>("#cartItemsWrapper")!;
-    wrapper.innerHTML = `
+  const wrapper = document.querySelector<HTMLDivElement>("#cartItemsWrapper")!;
+  wrapper.innerHTML = `
     <div>
     <h2>🛍️ Tack för din order! 🛍️</h2>
     <p>Ditt ordernummer är: ${data.data.id}</p>
     </div>
-    `
-}
+    `;
+};
 
 // funktion som renderar HTML för att ordern inte gick igenom
 const renderStatusFail = () => {
-    const wrapper = document.querySelector<HTMLDivElement>("#cartItemsWrapper")!;
-    wrapper.innerHTML = `
+  const wrapper = document.querySelector<HTMLDivElement>("#cartItemsWrapper")!;
+  wrapper.innerHTML = `
     <div>
     <h2>Din order kunde inte skickas</h2>
     <p>Felet ligger hos vår leverantör och de är medvetna om felet</p>
     <p>LÄGG IN EN GIF</p>
     </div>
-    `
-}
+    `;
+};

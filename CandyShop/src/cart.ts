fd336b23ee900719324e4d72b725cd0649e3d69c
Oscar Throedsson <oscar.throedsson@gmail.com>
2024-01-23 14:08:41 +0100

    added prettierrc file

--- CandyShop/src/cart.ts
+++ CandyShop/src/cart.ts
@@ -1,7 +1,4 @@
-import {
-  getCart,
-  adjustCart,removeFromCart
-} from "./localStorageLogic";
+import { getCart, adjustCart, removeFromCart } from "./localStorageLogic";
 import { renderOrder } from "./placeOrder";
 import { CartItem } from "./interface";
 import "bootstrap/dist/css/bootstrap.css";
@@ -9,42 +6,42 @@ import { setListeners } from "./eventListners";
 
 // Öppna aside som innehåller kassan
 export function cartListener() {
-  const mainEl = document.querySelector<HTMLDivElement>("#app")!;
-  const cartIcon = document.querySelector<HTMLDivElement>(".bajs")!;
+	const mainEl = document.querySelector<HTMLDivElement>("#app")!;
+	const cartIcon = document.querySelector<HTMLDivElement>(".bajs")!;
 
-  // Lyssnare för att rendera varukorgen
-  cartIcon.addEventListener("click", () => {
-    mainEl.innerHTML += `<aside id="sideWindow"></aside>`
-    renderCart()
-  })
+	// Lyssnare för att rendera varukorgen
+	cartIcon.addEventListener("click", () => {
+		mainEl.innerHTML += `<aside id="sideWindow"></aside>`;
+		renderCart();
+	});
 }
 
 // UIn för att rendera ut asiden!
 const renderCart = () => {
-  const aside = document.querySelector<HTMLDivElement>("#sideWindow")!;
-  const cartItems: CartItem[] = getCart();
-   
-   if(cartItems.length <1){
-    aside.innerHTML = `
+	const aside = document.querySelector<HTMLDivElement>("#sideWindow")!;
+	const cartItems: CartItem[] = getCart();
+
+	if (cartItems.length < 1) {
+		aside.innerHTML = `
     <div class="emptyCart">
     <p>Tyvärr har du inget i din kundvagn</p>
     <button class="btn btnText closeCartBtn">Handla lite</button>
     </div>
-    `
-   }else{
-    // Totala summan för alla produkter
-   let totalPrice: number = 0;
-   cartItems?.forEach((total) => {
-    totalPrice += total.totalCost;
-   });
- 
-   // Totala antalet produkter både av samma och olika
-   let totalProduct: number = 0;
-   cartItems?.forEach((total) => {
-    totalProduct += total.amount;
-   });
+    `;
+	} else {
+		// Totala summan för alla produkter
+		let totalPrice: number = 0;
+		cartItems?.forEach((total) => {
+			totalPrice += total.totalCost;
+		});
 
-    aside.innerHTML=`
+		// Totala antalet produkter både av samma och olika
+		let totalProduct: number = 0;
+		cartItems?.forEach((total) => {
+			totalProduct += total.amount;
+		});
+
+		aside.innerHTML = `
     <header class="cartHeader">
         <button class="btn btnIcon closeCartBtn">
         <i class="bi bi-x-square"></i>
@@ -55,8 +52,8 @@ const renderCart = () => {
   <h2 class="headerKassa">Kundvagn</h2>
       <ul>
        ${cartItems
-        ?.map((cartItem: CartItem) => {
-          return `
+			?.map((cartItem: CartItem) => {
+				return `
                      <li id="candyCard"> 
                       <div class="itemCard">
                           <img src="${cartItem.image}" alt=""/> 
@@ -76,9 +73,9 @@ const renderCart = () => {
                           </div>
                           <button class="eraseProduct" value="${cartItem.id}"><i class="bi bi-trash"></i></button>
                       </div>
-                    </li>`
-        })
-        .join("")}
+                    </li>`;
+			})
+			.join("")}
       </ul>
     </div>
 
@@ -89,7 +86,7 @@ const renderCart = () => {
           Antal godisar: ${totalProduct} st
         </li>
         <li class="smallText">
-          skatt: ${Number(totalPrice*0.2).toFixed(0)} kr
+          skatt: ${Number(totalPrice * 0.2).toFixed(0)} kr
         </li>
         <li>
           Att betala:${totalPrice} kr
@@ -97,75 +94,78 @@ const renderCart = () => {
         </ul>
         <button id="checkout" class="btn btnText">Checkout</button>
     </div>
-    `
-      }
+    `;
+	}
 
-    closeCart()
-    adjustCandyItems()
-    getIdToRemove();
-    checkout();
-}
+	closeCart();
+	adjustCandyItems();
+	getIdToRemove();
+	checkout();
+};
 
 // Funktion för att stänga varukorgen om användaren vill kolla mer i godisaffären
 function closeCart() {
-  const buttonCartEl =
-    document.querySelector<HTMLButtonElement>(".closeCartBtn")!;
-  buttonCartEl?.addEventListener("click", () => {
-    const cartItemsWrapperEl =
-      document.querySelector<HTMLDivElement>("#sideWindow")!;
-    cartItemsWrapperEl.remove();
-    cartListener()
-    setListeners();
-  });
+	const buttonCartEl =
+		document.querySelector<HTMLButtonElement>(".closeCartBtn")!;
+	buttonCartEl?.addEventListener("click", () => {
+		const cartItemsWrapperEl =
+			document.querySelector<HTMLDivElement>("#sideWindow")!;
+		cartItemsWrapperEl.remove();
+		cartListener();
+		setListeners();
+	});
 }
 
 // Funktion som tar användaren till formuläret för att fylla in sin data
-function checkout(){
-  const checkoutEl = document.querySelector<HTMLFormElement>("#checkout");
-  checkoutEl?.addEventListener("click", () => {
-    renderOrder();
-    const sumTable = document.querySelector<HTMLDivElement>(".sumTable");
-    sumTable?.remove()
-    
-  });
+function checkout() {
+	const checkoutEl = document.querySelector<HTMLFormElement>("#checkout");
+	checkoutEl?.addEventListener("click", () => {
+		renderOrder();
+		const sumTable = document.querySelector<HTMLDivElement>(".sumTable");
+		sumTable?.remove();
+	});
 }
 
 // Funktion för att ändra antalet godisar i varukorgen
 function adjustCandyItems() {
-  const increaseCandy = document.querySelectorAll(".increaseCandy");
-  const decreaseCandy = document.querySelectorAll(".decreaseCandy");
+	const increaseCandy = document.querySelectorAll(".increaseCandy");
+	const decreaseCandy = document.querySelectorAll(".decreaseCandy");
 
-   // Öka antalet godisar
-  increaseCandy.forEach((increaseBtn) => {
-    increaseBtn.addEventListener("click", () => {
-      if(increaseBtn.nextElementSibling){
-        const id: number = Number(increaseBtn.nextElementSibling.getAttribute("value"));
-        adjustCart(id, "add");
-        renderCart()
-      }
-    });
-  });
+	// Öka antalet godisar
+	increaseCandy.forEach((increaseBtn) => {
+		increaseBtn.addEventListener("click", () => {
+			if (increaseBtn.nextElementSibling) {
+				const id: number = Number(
+					increaseBtn.nextElementSibling.getAttribute("value")
+				);
+				adjustCart(id, "add");
+				renderCart();
+			}
+		});
+	});
 
-  // Minska antalet godisar
-  decreaseCandy.forEach((decreaseBtn) => {
-  decreaseBtn.addEventListener("click", () => {
-    if(decreaseBtn.previousElementSibling){
-      const id: number = Number(decreaseBtn.previousElementSibling.getAttribute("value"));
-      adjustCart(id, "remove");
-      renderCart()
-    }
-  });
-});
+	// Minska antalet godisar
+	decreaseCandy.forEach((decreaseBtn) => {
+		decreaseBtn.addEventListener("click", () => {
+			if (decreaseBtn.previousElementSibling) {
+				const id: number = Number(
+					decreaseBtn.previousElementSibling.getAttribute("value")
+				);
+				adjustCart(id, "remove");
+				renderCart();
+			}
+		});
+	});
 }
 
 // Tar bort direkt från kundvagnen
 function getIdToRemove() {
-  const allCartItems = document.querySelectorAll(".eraseProduct");
-  allCartItems.forEach((element)=>{
-    element.addEventListener("click", ()=>{
-      const candyID = element.getAttribute("value");
-      removeFromCart(Number(candyID))
-      renderCart()
-    })
-  })
+	const allCartItems = document.querySelectorAll(".eraseProduct");
+	allCartItems.forEach((element) => {
+		element.addEventListener("click", () => {
+			const candyID = element.getAttribute("value");
+			removeFromCart(Number(candyID));
+			renderCart();
+		});
+	});
 }
--- CandyShop/src/placeOrder.ts
+++ CandyShop/src/placeOrder.ts
@@ -4,37 +4,38 @@ import { sendOrder } from "./apiCalls";
 
 // Funktion för att skapa varje objekt som ska skickas med POST till API:et
 function objApi() {
-  const cart = getCart();
-  const orderItems = cart?.map((product) => {
-    return {
-      product_id: product.id,
-      qty: product.amount,
-      item_price: product.price,
-      item_total: product.totalCost,
-    };
-  });
-  return orderItems;
+	const cart = getCart();
+	const orderItems = cart?.map((product) => {
+		return {
+			product_id: product.id,
+			qty: product.amount,
+			item_price: product.price,
+			item_total: product.totalCost
+		};
+	});
+	return orderItems;
 }
 
 // Funktion för att rendera orderinfo + formulär
 export function renderOrder() {
-  const wrapper = document.querySelector<HTMLDivElement>("#cartItemsWrapper")!;
-  const cartItems = getCart();
+	const wrapper =
+		document.querySelector<HTMLDivElement>("#cartItemsWrapper")!;
+	const cartItems = getCart();
 
-  // Räkna ut total summan av alla godisar
-  let totalPrice: number = 0;
-  cartItems?.forEach((total) => {
-    totalPrice += total.totalCost;
-  });
+	// Räkna ut total summan av alla godisar
+	let totalPrice: number = 0;
+	cartItems?.forEach((total) => {
+		totalPrice += total.totalCost;
+	});
 
-  // Räkna ut antalet av alla godisar
-  let totalProduct: number = 0;
-  cartItems?.forEach((total) => {
-    totalProduct += total.amount;
-  });
+	// Räkna ut antalet av alla godisar
+	let totalProduct: number = 0;
+	cartItems?.forEach((total) => {
+		totalProduct += total.amount;
+	});
 
-  // HTML för att rendera ett formulär i kassan
-  wrapper.innerHTML = `
+	// HTML för att rendera ett formulär i kassan
+	wrapper.innerHTML = `
 <header>
 <h2 class="headerKassa">Kassa</h2>
 <p>Fyll i dina uppgifter:</p>
@@ -80,88 +81,96 @@ export function renderOrder() {
  <button class="btn btnText" type="submit">Lägg order</button>
 </form>`;
 
-  placeOrder(totalPrice);
+	placeOrder(totalPrice);
 }
 
 // Funktion för att skicka order med inputvärderna till api
 function placeOrder(totalPrice: number) {
-  const orderFormEl = document.querySelector<HTMLFormElement>(".orderForm");
+	const orderFormEl = document.querySelector<HTMLFormElement>(".orderForm");
 
-  // Eventlyssnare som lyssar på att användaren lägger en order
-  orderFormEl?.addEventListener("submit", async (e) => {
-    e.preventDefault();
+	// Eventlyssnare som lyssar på att användaren lägger en order
+	orderFormEl?.addEventListener("submit", async (e) => {
+		e.preventDefault();
 
-    // Alla DOM refernenser
-    const firstNameEl = document.querySelector<HTMLInputElement>("#firstName");
-    const lastNameEl = document.querySelector<HTMLInputElement>("#lastName");
-    const adressInputEl =
-      document.querySelector<HTMLInputElement>("#adressInput");
-    const zipcodeInputEl =
-      document.querySelector<HTMLInputElement>("#zipcodeInput");
-    const cityInputEl = document.querySelector<HTMLInputElement>("#cityInput");
-    const mailInputEl = document.querySelector<HTMLInputElement>("#mailInput");
+		// Alla DOM refernenser
+		const firstNameEl =
+			document.querySelector<HTMLInputElement>("#firstName");
+		const lastNameEl =
+			document.querySelector<HTMLInputElement>("#lastName");
+		const adressInputEl =
+			document.querySelector<HTMLInputElement>("#adressInput");
+		const zipcodeInputEl =
+			document.querySelector<HTMLInputElement>("#zipcodeInput");
+		const cityInputEl =
+			document.querySelector<HTMLInputElement>("#cityInput");
+		const mailInputEl =
+			document.querySelector<HTMLInputElement>("#mailInput");
 
-    // Värderna för alla inputfält
-    const firstName = firstNameEl?.value || "";
-    const lastName = lastNameEl?.value || "";
-    const adressInput = adressInputEl?.value || "";
-    const zipcodeInput = zipcodeInputEl?.value || "";
-    const cityInput = cityInputEl?.value || "";
-    const mailInput = mailInputEl?.value || "";
+		// Värderna för alla inputfält
+		const firstName = firstNameEl?.value || "";
+		const lastName = lastNameEl?.value || "";
+		const adressInput = adressInputEl?.value || "";
+		const zipcodeInput = zipcodeInputEl?.value || "";
+		const cityInput = cityInputEl?.value || "";
+		const mailInput = mailInputEl?.value || "";
 
-    const cart = objApi();
+		const cart = objApi();
 
-    // Skapar ett objekt med beställaren inputs och varor som ska köpas
-    const placeOrder: Data = {
-      customer_first_name: firstName,
-      customer_last_name: lastName,
-      customer_address: adressInput,
-      customer_postcode: zipcodeInput,
-      customer_city: cityInput,
-      customer_email: mailInput,
-      order_total: totalPrice,
-      order_items: cart,
-    };
+		// Skapar ett objekt med beställaren inputs och varor som ska köpas
+		const placeOrder: Data = {
+			customer_first_name: firstName,
+			customer_last_name: lastName,
+			customer_address: adressInput,
+			customer_postcode: zipcodeInput,
+			customer_city: cityInput,
+			customer_email: mailInput,
+			order_total: totalPrice,
+			order_items: cart
+		};
 
-    // Kollar att alla requiered inputfält är ifyllda
-    if (
-      !firstName ||
-      !lastName ||
-      !adressInput ||
-      !zipcodeInput ||
-      !cityInput ||
-      !mailInput
-    ) {
-      alert("Please fill in all required fields");
-      return;
-    }
+		// Kollar att alla requiered inputfält är ifyllda
+		if (
+			!firstName ||
+			!lastName ||
+			!adressInput ||
+			!zipcodeInput ||
+			!cityInput ||
+			!mailInput
+		) {
+			alert("Please fill in all required fields");
+			return;
+		}
 
-    // Försöker göra en POST till API + Catch hanterar om det inte går att anropa APIet
-    try {
-      const response = await sendOrder(placeOrder);
-      renderStatusSuccess(response);
-    } catch (error) {
-      renderStatusFail();
-    }
-  });
+		// Försöker göra en POST till API + Catch hanterar om det inte går att anropa APIet
+		try {
+			const response = await sendOrder(placeOrder);
+			renderStatusSuccess(response);
+		} catch (error) {
+			renderStatusFail();
+		}
+	});
 }
 
 // Funktion som renderar HTML om ordern gick igenom
 const renderStatusSuccess = (data: ApiResponse) => {
-  const wrapper = document.querySelector<HTMLDivElement>("#cartItemsWrapper")!;
-  wrapper.innerHTML = `
+	const wrapper =
+		document.querySelector<HTMLDivElement>("#cartItemsWrapper")!;
+	wrapper.innerHTML = `
     <div class="successwrapper">
     <h2 class="headerKassa">🛍️ Tack för din order! 🛍️</h2>
     <p class="ordernummer">Ditt ordernummer är: ${data.data.id}</p>
     <p>${data.data.order_date}</p>
     </div>
     `;
+
+	//RENDERA KUNDVAGNENS HTML MEN TA BORT KNAPPARNA
 };
 
 // Funktion som renderar HTML om ordern inte gick igenom
 const renderStatusFail = () => {
-  const wrapper = document.querySelector<HTMLDivElement>("#cartItemsWrapper")!;
-  wrapper.innerHTML = `
+	const wrapper =
+		document.querySelector<HTMLDivElement>("#cartItemsWrapper")!;
+	wrapper.innerHTML = `
     <div class="successwrapper">
     <h2 class="headerKassa">Din order kunde inte skickas</h2>
     <p>Felet ligger hos vår leverantör och de är medvetna om felet</p>
